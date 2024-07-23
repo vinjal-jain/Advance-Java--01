d1550633aa9baca0c0fdad8f7877b1c184b6a954
vinjal-jain <vinjaljain14it@gmail.com>
2024-07-23 12:38:32 +0530

    dynamic_conncept

---
+++ marksheet/MarksheetBean.java
@@ -0,0 +1,43 @@
+package in.co.rays.marksheet;
+
+public class MarksheetBean {
+	
+	private int roll_no;
+	private String name;
+	private int physics;
+	private int chemistry;
+	private int mathematics;
+	public int getRoll_no() {
+		return roll_no;
+	}
+	public void setRoll_no(int roll_no) {
+		this.roll_no = roll_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhysics() {
+		return physics;
+	}
+	public void setPhysics(int physics) {
+		this.physics = physics;
+	}
+	public int getChemistry() {
+		return chemistry;
+	}
+	public void setChemistry(int chemistry) {
+		this.chemistry = chemistry;
+	}
+	public int getMathematics() {
+		return mathematics;
+	}
+	public void setMathematics(int mathematics) {
+		this.mathematics = mathematics;
+	
+	}
+}
+	
+
